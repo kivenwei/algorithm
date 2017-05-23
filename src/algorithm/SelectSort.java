3cc1314d3caa2578cb545e742488eea1207411d9
kivenwei <kivenwei@outlook.com>
2017-05-23 18:19:19 +0800

    修改中间记录

--- src/algorithm/SelectSort.java
+++ src/algorithm/SelectSort.java
@@ -6,6 +6,7 @@ public class SelectSort {
 		// TODO Auto-generated method stub
 		int[] array = new int[]{8,3,2,1,7,4,6,5};
 		printResult(array);
+		int count = 0;
 		for(int i = 0; i < array.length-1; i++)
 		{
 			for(int j = i + 1; j < array.length; j++)
@@ -13,11 +14,13 @@ public class SelectSort {
 				if(array[i] > array[j])
 				{
 					swap(array, i , j);
+					count++;
 				}
+				printResult(array);
 			}
 		}
 		printResult(array);
-
+		System.out.println(count);
 	}
 	
 	private static void printResult(int[] array){

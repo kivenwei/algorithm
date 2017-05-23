b217518b6b3401345d442f3c3ddc3be1866d117c
kivenwei <kivenwei@outlook.com>
2017-05-23 17:55:52 +0800

    添加algorithm project

---
+++ src/algorithm/SelectSort.java
@@ -0,0 +1,36 @@
+package algorithm;
+
+public class SelectSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[]{8,3,2,1,7,4,6,5};
+		printResult(array);
+		for(int i = 0; i < array.length-1; i++)
+		{
+			for(int j = i + 1; j < array.length; j++)
+			{
+				if(array[i] > array[j])
+				{
+					swap(array, i , j);
+				}
+			}
+		}
+		printResult(array);
+
+	}
+	
+	private static void printResult(int[] array){
+		for(int value:array){
+			System.out.print(" " + value + " ");
+		}
+		System.out.println();
+	}
+	
+	private static void swap(int[] array, int i, int j){
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+}

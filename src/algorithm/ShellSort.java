c85a1df574893933289ece6cc8cc4b81ca22dce5
kivenwei <kivenwei@outlook.com>
2017-05-26 08:18:10 +0800

    修改shell排序

--- src/algorithm/ShellSort.java
+++ src/algorithm/ShellSort.java
@@ -6,6 +6,21 @@ public class ShellSort {
 		// TODO Auto-generated method stub
 		int[] array = new int[]{8,3,2,1,7,4,6,5};
 		printResult(array);
+		int gap = array.length/2;
+		while(1 <= gap)
+		{
+			for(int i = gap; i < array.length; i++)
+			{
+				int j = 0;
+				int temp = array[i];
+				for(j = i - gap; j >= 0 && temp < array[j]; j = j - gap )
+				{
+					array[j + gap] = array[j];
+				}
+				array[j + gap] = temp;
+			}
+			gap = gap/2;
+		}
 		
 		printResult(array);
 

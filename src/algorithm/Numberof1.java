ddd24560996cab32b3be729f53fb4dace61ac01b
kivenwei <kivenwei@outlook.com>
2017-11-28 10:54:00 +0800

    calculate 1 number

---
+++ src/algorithm/Numberof1.java
@@ -0,0 +1,21 @@
+package com.lintcode;
+
+import org.omg.CORBA.WCharSeqHelper;
+
+public class Numberof1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println(number1(9));
+	}
+	
+	public static int number1(int n){
+		int count = 0;
+		while(n != 0){
+			count++;
+			n = n & (n-1);
+		}
+		return count;
+	}
+
+}
